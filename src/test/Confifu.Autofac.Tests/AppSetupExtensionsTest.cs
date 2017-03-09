bc6c4c497f1761ef60336be2d96f2aa03c9e766c
ivanbenko <bivans209@gmail.com>
2017-03-09 14:53:40 +0100

    new .net core tools 1.0, vs2017, added tests, v2.0.0

---
+++ src/test/Confifu.Autofac.Tests/AppSetupExtensionsTest.cs
@@ -0,0 +1,74 @@
+using Confifu.Abstractions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Xunit;
+using Shouldly;
+
+namespace Confifu.Autofac.Tests
+{
+    public class AppSetupExtensionsTest
+    {
+        [Fact]
+        public void it_throws_exception_if_there_is_no_service_collection()
+        {
+            var appConfig = new AppConfig();
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                appConfig.SetupAutofacContainer();
+            });   
+        }
+
+        [Fact]
+        public void it_creates_valid_service_provider()
+        {
+            var appConfig = new AppConfig();
+
+            var sc = new ServiceCollection();
+
+            sc.AddTransient<IServiceA, ServiceA>();
+            sc.AddTransient<IServiceB, ServiceB>();
+
+            appConfig.SetServiceCollection(sc);
+
+            appConfig.SetupAutofacContainer();
+
+            var sp = appConfig.GetServiceProvider();
+
+            var a = sp.GetService<IServiceA>();
+
+            a.ShouldNotBeNull();
+            a.AName().ShouldBe(new ServiceA().AName());
+
+            var b = sp.GetService<IServiceB>();
+            b.ShouldNotBeNull();
+
+            b.BName().ShouldBe(new ServiceB(new ServiceA()).BName());
+        }
+    }
+    public interface IServiceA {
+        string AName();
+    }
+
+    public interface IServiceB
+    {
+        string BName();
+    }
+
+    public class ServiceA : IServiceA
+    {
+        public string AName() => "ServiceA";
+    }
+
+    public class ServiceB : IServiceB
+    {
+        private IServiceA serviceA;
+
+        public ServiceB(IServiceA serviceA)
+        {
+            this.serviceA = serviceA;
+        }
+
+        public string BName() => $"{serviceA.AName()}ServiceB";
+    }
+}
